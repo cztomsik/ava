296bad71b4b9f5825939feec17c587a0ac83c51a
Kamil Tomsik <info@tomsik.cz>
2023-08-31 16:40:25 +0200

    fix prompts in examples

--- src/app/quick-tools/_examples.ts
+++ src/app/quick-tools/_examples.ts
@@ -8,6 +8,18 @@ export const examples = [
       The following is an excerpt from a story about Bob, the cat, flying to space.
     `,
   },
+
+  // TODO: Writing technical documentation, product announcement, etc.
+  {
+    title: "Copywriting",
+    description: "Copywriting",
+    prompt: dedent`
+      SYSTEM: User needs help with copywriting.
+      ASSISTANT: Hello.
+      USER: Write a blog post about {{subject}}.
+      ASSISTANT:
+    `,
+  },
 ]
 
 if (DEV) {
@@ -16,114 +28,114 @@ if (DEV) {
       title: "Grammar Police",
       description: "Correct grammar in the provided text",
       prompt: dedent`
-    User: Correct grammar in the following text:
-    {{text}}
-    Assistant: The correct grammar in the following text would be:
-  `,
+        USER: Correct grammar in the following text:
+        {{text}}
+        ASSISTANT: The correct grammar in the following text would be:
+      `,
     },
 
     {
       title: "Rephrase",
       description: "",
       prompt: dedent`
-    User: Rephrase the following text so it looks more professional:
-    {{text}}
-    Assistant:
-  `,
+        USER: Rephrase the following text so it looks more professional:
+        {{text}}
+        ASSISTANT:
+      `,
     },
 
     {
       title: "Vacation Planner",
       description: "Plan a vacation",
       prompt: dedent`
-    User: I need a help with planning a vacation to {{destination}}. I want to go there for {{days}} days. I want to go there in {{month}}. My budget is {{budget}} dollars. {{extra}}
-    Assistant: Here is the plan for your vacation:
-  `,
+        USER: I need a help with planning a vacation to {{destination}}. I want to go there for {{days}} days. I want to go there in {{month}}. My budget is {{budget}} dollars. {{extra}}
+        ASSISTANT: Here is the plan for your vacation:
+      `,
     },
 
     {
       title: "Writing Style",
       description: "Suggest style improvements",
       prompt: dedent`
-    User: Given the following text, suggest improvements to the style:
-    {{text}}
-    Assistant: The following improvements can be made: 
-  `,
+        USER: Given the following text, suggest improvements to the style:
+        {{text}}
+        ASSISTANT: The following improvements can be made: 
+      `,
     },
 
     {
       title: "Interview Prep",
       description: "Prepare for an interview",
       prompt: dedent`
-    User: I need to prepare for an interview for a {{position}} position at {{company}}. The company industry is {{industry}}.
-    Assistant: Here are the questions you should prepare for:
-  `,
+        USER: I need to prepare for an interview for a {{position}} position at {{company}}. The company industry is {{industry}}.
+        ASSISTANT: Here are the questions you should prepare for:
+      `,
     },
 
     {
       title: "Write an E-mail",
       description: "Write e-mail",
       prompt: dedent`
-    User: I need to write an e-mail to {{name}} about {{about}} because {{reason}}\nAssistant:
-  `,
+        USER: I need to write an e-mail to {{name}} about {{about}} because {{reason}}\nASSISTANT:
+      `,
     },
 
     {
       title: "Write a Reply",
       description: "Write e-mail reply",
       prompt: dedent`
-    User: I got this email from {{name}}:
-    {{email}}
-    Rephrase this reply in a more polite way:
-    {{reply}}
-    Assistant: I think you could write something like this:
-  `,
+        USER: I got this email from {{name}}:
+        {{email}}
+        Rephrase this reply in a more polite way:
+        {{reply}}
+        ASSISTANT: I think you could write something like this:
+      `,
     },
 
     {
       title: "Summarize",
       description: "Summarize text",
       prompt: dedent`
-    User: I need to summarize the following text:
-    {{text}}
-    Assistant: Here is the short summary as requested:
-  `,
+        USER: I need to summarize the following text:
+        {{text}}
+        ASSISTANT: Here is the short summary as requested:
+      `,
     },
 
     {
       title: "Buy or Sell",
       description: "Stock market advice",
       prompt: dedent`
-    User: Given the following text about a stock, should I buy or sell?
-    {{text}}
-    Assistant: Based on the given text, it is difficult to provide a definitive answer. However, given that investor's risk tolerance is high, I would suggest
-  `,
+        USER: Given the following text about a stock, should I buy or sell?
+        {{text}}
+        ASSISTANT: Based on the given text, it is difficult to provide a definitive answer. However, given that investor's risk tolerance is high, I would suggest
+      `,
     },
 
     {
       title: "Regex Help",
       description: "Write a regular expression",
       prompt: dedent`
-    User: I need a help writing regular expression in JavaScript. Specifically, I want to write regex for {{whatToMatch}}...
+        USER: I need a help writing regular expression in JavaScript. Specifically, I want to write regex for {{whatToMatch}}...
 
-    For example, the following should match:
-    {{example1}}
-    {{example2}}
+        For example, the following should match:
+        {{example1}}
+        {{example2}}
 
-    Only answer with regex and unit-tests.
+        Only answer with regex and unit-tests.
 
-    Assistant: You can use the following regular expression:
-  `,
+        ASSISTANT: You can use the following regular expression:
+      `,
     },
 
     {
       title: "Total Rewrite",
       description: "Reimplement given code in another language",
       prompt: dedent`
-    User: I need to reimplement the following code in {{language}}:
-    {{code}}
-    Assistant: Here is the code in {{language}}:
-  `,
+        USER: I need to reimplement the following code in {{language}}:
+        {{code}}
+        ASSISTANT: Here is the code in {{language}}:
+      `,
     }
   )
 }
