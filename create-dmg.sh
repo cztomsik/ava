dbbac3a9d4947800d0157786dabdafe61943f05f
Kamil Tomsik <info@tomsik.cz>
2023-08-31 09:38:28 +0200

    macos: add DMG script

---
+++ create-dmg.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+APP_NAME="Ava"
+APP_PATH="./zig-out/${APP_NAME}.app"
+DMG_TMP_PATH="./zig-out/${APP_NAME}_tmp.dmg"
+DMG_FINAL_PATH="./zig-out/${APP_NAME}.dmg"
+VOLUME_NAME="${APP_NAME}"
+MOUNT_PATH="/Volumes/${VOLUME_NAME}"
+
+# Check & clean
+if [ ! -d "${APP_PATH}" ]; then echo "Error: ${APP_PATH} doesn't exist!"; exit 1; fi
+if [ -f "${DMG_TMP_PATH}" ]; then rm "${DMG_TMP_PATH}"; fi
+if [ -f "${DMG_FINAL_PATH}" ]; then rm "${DMG_FINAL_PATH}"; fi
+
+# TODO: Code signing (self-signed first)
+# TODO: Notarization
+# codesign -fs cert-name --deep "${APP_PATH}"
+
+# Create temp DMG and perform some customizations
+# Adapted from https://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools
+hdiutil create -srcfolder "${APP_PATH}" -volname "${VOLUME_NAME}" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 512000k "${DMG_TMP_PATH}"
+MOUNT_RESULT=$(hdiutil attach -readwrite -noverify -noautoopen "${DMG_TMP_PATH}")
+DEVICE_PATH=$(echo "${MOUNT_RESULT}" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+echo '
+    tell application "Finder"
+        tell disk "'${APP_NAME}'"
+            open
+            set current view of container window to icon view
+            set the bounds of container window to {400, 100, 885, 430}
+            set theViewOptions to the icon view options of container window
+            set arrangement of theViewOptions to not arranged
+            set icon size of theViewOptions to 72
+            make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+            set position of item "'${APP_NAME}'" of container window to {100, 100}
+            set position of item "Applications" of container window to {375, 100}
+            update without registering applications
+            delay 5
+            close
+        end tell
+    end tell
+' | osascript
+hdiutil detach "${DEVICE_PATH}"
+
+# # # Create final DMG
+# # hdiutil convert "${DMG_TMP_PATH}" -format UDZO -imagekey zlib-level=9 -o "${DMG_FINAL_PATH}"
+# # rm "${DMG_TMP_PATH}"
+
+# # echo "DMG created at ${DMG_FINAL_PATH}"
