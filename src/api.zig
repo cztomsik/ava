1d600b751a0c2991cc73bb1ac6f1d2e32ec077a6
Kamil Tomsik <info@tomsik.cz>
2023-09-02 22:41:14 +0200

    WIP: status reporting

--- src/api.zig
+++ src/api.zig
@@ -29,19 +29,22 @@ pub fn handler(ctx: *server.Context) !void {
         var sampler = llama.Sampler.init(ctx.arena, params.sampling);
         defer sampler.deinit();
 
+        try ctx.sendJson(.{ .status = "Waiting for the model..." });
         var cx = try llama.Pool.get(try registry.getModelPath(ctx.arena, params.model));
         defer llama.Pool.release(cx);
 
+        try ctx.sendJson(.{ .status = "Reading the history..." });
         try cx.prepare(params.prompt);
 
         // Multi-byte, TODO: move to Sampler?
         var buf = std.ArrayList(u8).init(ctx.arena);
 
+        // TODO: sometimes the model gets stuck with token 23 - maybe repetition penalty could help?
         while (try cx.generate(&sampler)) |token| {
             try buf.appendSlice(cx.model.token_to_str(token));
 
             if (std.unicode.utf8ValidateSlice(buf.items)) {
-                try ctx.sendChunk(buf.items);
+                try ctx.sendJson(.{ .content = buf.items });
                 buf.clearRetainingCapacity();
             }
         }
