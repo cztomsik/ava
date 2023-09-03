0f007cd0da0c21665b09f90202b6318522946d66
Kamil Tomsik <info@tomsik.cz>
2023-09-03 12:23:22 +0200

    Markdown needs to be recursive even for basic formatting

--- src/app/_components/Markdown.tsx
+++ src/app/_components/Markdown.tsx
@@ -42,13 +42,13 @@ const parse = (input, target = []) => {
   }
 
   for (input = input.trim(), pos = 0; pos < input.length; match = null) {
-    // Basic formatting (intentionally non-recursive)
+    // Basic formatting
     p(/\\(.)/g, ch => ch) // Escape
     p(/(  |\\)\n/g, () => <br />) // Line ends with 2 spaces or a backslash
     p(/^(---|\*\*\*|___)\n/gm, () => <hr />)
-    p(/(__|\*\*)(.*?)\1/g, (_, text) => <strong class="font-bold">{text}</strong>)
-    p(/(_|\*)(.*?)\1/g, (_, text) => <em class="font-italic">{text}</em>)
-    p(/~~(.*?)~~/g, text => <s>{text}</s>)
+    p(/(__|\*\*)(.*?)\1/g, (_, text) => <strong class="font-bold">{parse(text)}</strong>)
+    p(/(_|\*)(.*?)\1/g, (_, text) => <em class="font-italic">{parse(text)}</em>)
+    p(/~~(.*?)~~/g, text => <s>{parse(text)}</s>)
     p(/!\[(.*?)\]\((.*?)(\s.*)?\)/g, (alt, src) => <img src={src} alt={alt} />)
     p(/`([^`]+)`/g, text => <code class="font-mono">{text}</code>)
     p(/\[(.*?)\]\((.*?)\)/g, (children, href) => <a class="text-blue-500" {...{ href, children }} />)
