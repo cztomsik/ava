d5df671644b92c0720cae49dfba24025665da0df
Kamil Tomsik <info@tomsik.cz>
2023-09-11 09:23:48 +0200

    fix types

--- src/app/custom.d.ts
+++ src/app/custom.d.ts
@@ -1,3 +1,11 @@
+interface WeakRef<T> {
+  deref(): T | undefined
+}
+
+declare var WeakRef: {
+  new <T extends object>(target: T): WeakRef<T>
+}
+
 // so we don't need to typecast event.target every time
 interface EventTarget {
   value: any
