cf18a16e13896be303fad1951eeede255dda0ae5
Kamil Tomsik <info@tomsik.cz>
2023-09-12 12:49:40 +0200

    improve modal

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -1,18 +1,16 @@
-import { css } from "@twind/core"
-
 /**
  * Simple wrapper for Bootstrap modals.
  */
 export const Modal = ({ title, class: className = "", children, onClose }) => (
-  <div class={`fixed top-0 left-0 right-0 bottom-0 p-10 flex items-start justify-center ${className}`}>
-    <div class="rounded bg-neutral-50 border-1 border-neutral-500 shadow-lg w-1/2">
-      <div class="hstack px-4 pt-4">
+  <div tabIndex={-1} aria-hidden="true" class={`fixed inset-0 p-10 flex items-start justify-center ${className}`}>
+    <div class="rounded-lg bg-neutral-100 text-neutral-700 border-1 border-neutral-200 shadow-lg md:w-2/5">
+      <div class="hstack px-5 pt-5">
         <h5 class="text-lg font-medium">{title}</h5>
         <button type="button" class="ml-auto" onClick={onClose}>
           X
         </button>
       </div>
-      <div class="p-4">{children}</div>
+      <div class="p-5">{children}</div>
     </div>
   </div>
 )
