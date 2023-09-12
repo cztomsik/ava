8fbf0d329c8e0c71cc980a5f4ef3d5002cea1431
Kamil Tomsik <info@tomsik.cz>
2023-09-12 23:07:04 +0200

    chat-saving/loading, auto-create, etc.

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -1,47 +1,76 @@
-import { signal, useSignal } from "@preact/signals"
-import { GenerationProgress, PageContent, PageFooter, PageHeader } from "../_components"
+import { GenerationProgress, PageContent, PageFooter, PageHeader, Select } from "../_components"
 import { ChatLog } from "./ChatLog"
 import { ChatInput } from "./ChatInput"
-import { useGenerate } from "../_hooks"
+import { useApi, getApiContext, useGenerate } from "../_hooks"
+import { router } from "../router"
 
-export const Chat = () => {
-  const { generate, ...progress } = useGenerate()
-  const messages = useSignal([{ role: "system", content: defaultPrompt }])
+export const Chat = ({ params: { id } }) => {
+  const { post: createChat } = useApi("chat")
+  let { data: messages = [], post: pushMessage } = useApi(id && `chat/${id}/messages`)
+  const { generate, result, ...progress } = useGenerate()
+  const draft = { role: "assistant", content: result }
 
   const handleSend = async text => {
-    const content = signal("")
-    messages.value = [...messages.value, { role: "user", content: text }, { role: "assistant", content }]
+    if (!id) {
+      id = (await createChat({ name: `New chat ${new Date().toLocaleDateString()}` })).id
+      pushMessage = getApiContext(`chat/${id}/messages`).post
+      router.navigate(`/chat/${id}`, true)
+    }
+
+    const msg = { role: "user", content: text }
+    await pushMessage(msg)
 
     // TODO: use /tokenize because every model has its own tokenizer and this might work just by accident
-    for await (const temp of generate(serializePrompt(messages.value), { stop: ["USER", ":"] })) {
-      content.value = temp
+    for await (const x of generate(serializePrompt([...messages, msg, draft]), { stop: ["USER", ":"] })) {
+      // TODO: the generate keeps going even after the stop condition is met
+      console.log(x)
     }
+
+    await pushMessage(draft)
+    draft.content.value = ""
   }
 
   return (
     <>
-      <PageHeader title="Chat" />
-
+      <PageHeader title="Chat">
+        <ChatSelect value={id} />
+      </PageHeader>
       <PageContent>
-        <ChatLog
-          chat={{ messages }}
-          fallbackContent={
-            <div class="text-neutral-400">
-              <div>The conversation is empty.</div>
-              <ul class="list-disc mt-2 ml-4">
-                <li>Use the input below to start chatting.</li>
-                <li>You can type multi-line messages with Shift+Enter.</li>
-              </ul>
-            </div>
-          }
-        />
+        <div class="text(neutral-400 lg:lg) mb-4">{defaultPrompt}</div>
+
+        {messages.length === 0 && (
+          <div class="text-neutral-400">
+            <div>The conversation is empty.</div>
+            <ul class="list-disc mt-2 ml-4">
+              <li>Use the input below to start chatting.</li>
+              <li>Use the dropdown above to load a previous chat.</li>
+              <li>You can type multi-line messages with Shift+Enter.</li>
+            </ul>
+          </div>
+        )}
+
+        <ChatLog messages={messages} draft={progress.data.value && draft} />
         <GenerationProgress class="mt-4" {...progress} />
       </PageContent>
-
-      <PageFooter class="pt-0">
+      <PageFooter class="pt-2">
         <ChatInput onSend={handleSend} />
       </PageFooter>
     </>
+  )
+}
+
+const ChatSelect = ({ value }) => {
+  const { data } = useApi("chat")
+
+  return (
+    <Select value={value} onChange={e => router.navigate(`/chat/${e.target.value}`)}>
+      <option value="">Previous chats...</option>
+      {data?.map(({ id, name }) => (
+        <option key={id} value={id}>
+          {name}
+        </option>
+      ))}
+    </Select>
   )
 }
 
@@ -49,6 +78,4 @@ const defaultPrompt =
   "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n\n"
 
 const serializePrompt = messages =>
-  messages
-    .reduce((res, m) => res + (m.role === "system" ? m.content : `${m.role.toUpperCase()}: ${m.content}\n`), "")
-    .trimEnd()
+  defaultPrompt + messages.reduce((res, m) => res + `${m.role.toUpperCase()}: ${m.content}\n`, "").trimEnd()
