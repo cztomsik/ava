45f35c079fff2af10466df187f3b7dddd83e3478
Kamil Tomsik <info@tomsik.cz>
2023-09-14 22:23:24 +0200

    reduce page padding a bit

--- src/app/_components/Page.tsx
+++ src/app/_components/Page.tsx
@@ -1,7 +1,7 @@
 export const PageHeader = ({ title, children = null }) => {
   return (
     <header
-      class="flex(& wrap) gap-2 items-end px(4 lg:6) py-2 border(b-1 neutral-200 dark:neutral-700)"
+      class="flex(& wrap) gap-2 items-end px-3 py-2 lg:pl-6 border(b-1 neutral-200 dark:neutral-700)"
       data-drag-window
     >
       <h2 class="mt-2 mr-auto text(xl neutral(800 dark:300)) font-medium">{title}</h2>
@@ -12,9 +12,9 @@ export const PageHeader = ({ title, children = null }) => {
 }
 
 export const PageContent = ({ class: className = "", ...props }) => (
-  <div class={`vstack max-w-full overflow-x-hidden overflow-y-auto p-4 lg:(py-6 px-6) ${className}`} {...props} />
+  <div class={`vstack max-w-full overflow-x-hidden overflow-y-auto p-3 lg:pl-6 ${className}`} {...props} />
 )
 
 export const PageFooter = ({ class: className = "", ...props }) => (
-  <footer class={`p-4 lg:px-6 ${className}`} {...props} />
+  <footer class={`p-3 lg:pl-6 ${className}`} {...props} />
 )
