eb1a32c3cb6e86d90b1e950f7a864cb2cfd211f3
Kamil Tomsik <info@tomsik.cz>
2023-09-15 21:49:33 +0200

    fix <Modal z-index

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -11,7 +11,11 @@ export const Modal = ({ title, class: className = "", children, onClose }) => {
   }, [])
 
   return (
-    <div tabIndex={-1} aria-hidden="true" class={`fixed inset-0 p-10 flex items-start justify-center ${className}`}>
+    <div
+      tabIndex={-1}
+      aria-hidden="true"
+      class={`fixed inset-0 z-50 p-10 flex items-start justify-center ${className}`}
+    >
       <div class="rounded-lg bg-neutral-100 text-neutral-700 border-1 border-neutral-300 shadow-lg md:w-2/5">
         <div class="hstack px-5 pt-5">
           <h5 class="text-lg font-medium">{title}</h5>
