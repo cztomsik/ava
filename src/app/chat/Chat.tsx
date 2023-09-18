0aaaa547288766be917007ca33f9e28b5a97a6df
Kamil Tomsik <info@tomsik.cz>
2023-09-18 12:57:05 +0200

    update instructions

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -40,6 +40,7 @@ export const Chat = ({ params: { id } }) => {
           <div class="text-neutral-500 bg-neutral-50 -mx-6 p-6 px-8 border(y-1 neutral-200)">
             <strong>The conversation is empty.</strong>
             <ul class="list-disc mt-2 ml-4">
+              <li>Select model in the bottom left corner.</li>
               <li>Use the input below to start chatting.</li>
               <li>Use the dropdown above to load a previous chat.</li>
               <li>You can type multi-line messages with Shift+Enter.</li>
