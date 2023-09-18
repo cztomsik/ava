f05a2ea954366c928b9bfcd7000ec9d8c3d5cdd5
Kamil Tomsik <info@tomsik.cz>
2023-09-18 16:55:18 +0200

    fix alert bg

--- src/app/_components/Alert.tsx
+++ src/app/_components/Alert.tsx
@@ -1,6 +1,6 @@
 export const Alert = ({ class: className = "", children }) => {
   return (
-    <div className={`p-3 rounded-md bg(warning-1) border(1 warning-6) text-warning(11) ${className}`} role="alert">
+    <div className={`p-3 rounded-md bg(warning-2) border(1 warning-6) text-warning(11) ${className}`} role="alert">
       {children}
     </div>
   )
