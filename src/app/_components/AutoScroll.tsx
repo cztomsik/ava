3bb2bca2f16fabfb6f213ea1ed823ab388da32fe
Kamil Tomsik <info@tomsik.cz>
2023-09-20 11:22:53 +0200

    fix scroll again

--- src/app/_components/AutoScroll.tsx
+++ src/app/_components/AutoScroll.tsx
@@ -1,31 +1,44 @@
+import { useSignal } from "@preact/signals"
 import { useEffect, useRef } from "preact/hooks"
 
 /**
  * Automatically scrolls to the bottom of the parent element.
  */
 export const AutoScroll = () => {
+  const enabled = useSignal(true)
+  const inView = useSignal(false)
   const ref = useRef(null)
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (!entry.isIntersecting) {
-          keepScrollingUntilInView(entry.target as HTMLElement)
-        }
-      },
-      { root: ref.current.parentElement, threshold: [0, 1] }
-    )
+    console.log("mounted")
+
+    const root = ref.current.parentElement
+
+    const handleScroll = () => {
+      // enabled.value = root.scrollTop + root.clientHeight >= root.scrollHeight - 10
+    }
+
+    const handleIntersect = ([entry]: IntersectionObserverEntry[]) => {
+      inView.value = entry.isIntersecting
+      keepScrollingUntilInView(entry.target as HTMLElement)
+    }
+
+    const keepScrollingUntilInView = (element: HTMLElement) => {
+      if (!inView.value) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" })
+        setTimeout(() => keepScrollingUntilInView(element), 1_000)
+      }
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, { root, threshold: [0, 1] })
+    root.addEventListener("scroll", handleScroll)
     observer.observe(ref.current)
 
-    return () => observer.disconnect()
+    return () => {
+      root.removeEventListener("scroll", handleScroll)
+      observer.disconnect()
+    }
   }, [])
 
   return <div class="min-h-[1px]" ref={ref} />
-}
-
-const keepScrollingUntilInView = (element: HTMLElement) => {
-  if (element.parentElement.scrollTop < element.offsetTop) {
-    element.scrollIntoView({ behavior: "smooth", block: "end" })
-    setTimeout(() => keepScrollingUntilInView(element), 100)
-  }
 }
