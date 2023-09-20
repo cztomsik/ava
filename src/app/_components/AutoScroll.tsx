dead0e9ffa9491d1f02329c82bc8434c64c40e66
Kamil Tomsik <info@tomsik.cz>
2023-09-20 12:22:58 +0200

    fix autoscroll in both safari and chrome

--- src/app/_components/AutoScroll.tsx
+++ src/app/_components/AutoScroll.tsx
@@ -2,7 +2,9 @@ import { useSignal } from "@preact/signals"
 import { useEffect, useRef } from "preact/hooks"
 
 /**
- * Automatically scrolls to the bottom of the parent element.
+ * Automatically scrolls to the bottom of the parent element. If the user
+ * scrolls up, auto-scrolling is disabled until the user scrolls to the bottom
+ * again.
  */
 export const AutoScroll = () => {
   const enabled = useSignal(true)
@@ -18,35 +20,35 @@ export const AutoScroll = () => {
         // Disable auto-scrolling when scrolling up
         enabled.value = false
       }
-    }
 
-    const handleScrollEnd = e => {
-      if (enabled.value && !inView.value) {
-        // Keep scrolling to the bottom if still not in view
-        el.scrollIntoView()
-      }
-
-      // Re-enable auto-scrolling if this was triggered by the user
-      if (root.scrollHeight - root.scrollTop - root.clientHeight <= 200) {
-        enabled.value = true
-      }
+      setTimeout(() => {
+        // Re-enable auto-scrolling if this was triggered by the user
+        if (root.scrollHeight - root.scrollTop - root.clientHeight <= 200) {
+          enabled.value = true
+        }
+      }, 100)
     }
 
     const handleIntersect = ([entry]: IntersectionObserverEntry[]) => {
       inView.value = entry.isIntersecting
-      if (enabled.value) {
+      scrollUntilInView()
+    }
+
+    // Safari does not have a scrollend event, so we need to use a timeout
+    const scrollUntilInView = () => {
+      if (enabled.value && !inView.value) {
+        // Chrome does not like { behavior: "smooth", block: "start" }
         el.scrollIntoView()
+        setTimeout(() => scrollUntilInView, 1_000)
       }
     }
 
     const observer = new IntersectionObserver(handleIntersect, { root, threshold: [0, 1] })
     root.addEventListener("wheel", handleWheel)
-    root.addEventListener("scrollend", handleScrollEnd)
     observer.observe(ref.current)
 
     return () => {
       root.removeEventListener("wheel", handleWheel)
-      root.removeEventListener("scrollend", handleScrollEnd)
       observer.disconnect()
     }
   }, [])
