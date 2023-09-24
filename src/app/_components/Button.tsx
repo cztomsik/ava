b334b0eabe73e3e4c3f3447f11f6214489cac283
Kamil Tomsik <info@tomsik.cz>
2023-09-24 21:26:04 +0200

    make inputs & buttons the same height

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -3,7 +3,7 @@ import { Link } from "./Link"
 export const Button = ({ class: className = "", submit = false, primary = submit, ...props }) => {
   props.type = submit ? "submit" : "button"
 
-  props.class = `border rounded-md inline-flex items-center justify-center px-3 py-1.5 text-sm bg-gradient-to-b ${
+  props.class = `border rounded-md inline-flex items-center justify-center px-3 py-[7px] text-sm bg-gradient-to-b ${
     primary
       ? "bg-primary(10 dark:11) from-primary(8 dark:9) text-sky(1 dark:4) border-primary(8 dark:10)"
       : "bg-neutral-5 from(white dark:neutral-11) text-neutral-12 border-neutral-6 shadow-thin"
