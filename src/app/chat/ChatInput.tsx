be4f8d9181c3e6c6bf1916a7aa0b51d12d00f074
Kamil Tomsik <info@tomsik.cz>
2023-09-25 10:37:50 +0200

    fix/reimplement auto-grow

--- src/app/chat/ChatInput.tsx
+++ src/app/chat/ChatInput.tsx
@@ -1,34 +1,54 @@
+import { css } from "@twind/core"
 import { useSignal } from "@preact/signals"
+import { useCallback } from "preact/hooks"
 import { Button, Form } from "../_components"
 
 export const ChatInput = ({ onSend }) => {
   const text = useSignal("")
 
-  const handleSubmit = e => {
+  const handleSubmit = useCallback(() => {
     onSend(text.value.trim())
     text.value = ""
-  }
+  }, [onSend])
 
-  const handleKeyDown = e => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault()
-      handleSubmit(e)
-    }
-  }
+  const handleKeyDown = useCallback(
+    e => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault()
+        handleSubmit()
+      }
+    },
+    [onSend]
+  )
 
   return (
-    <Form class="flex relative" onSubmit={handleSubmit}>
+    <Form
+      class={`vstack relative p-2 pr-16 ${autoGrow}`}
+      style="max-height: 30vh"
+      onSubmit={handleSubmit}
+      data-value={text}
+    >
       <textarea
-        class="w-full py-2 pr-10 bg-transparent"
-        rows={Math.min(6, text.value.split("\n").length)}
+        class="absolute inset-0 py-2 pr-16 bg-transparent"
+        rows={1}
         placeholder="Ask anything..."
-        value={text.value}
+        value={text}
         onInput={e => (text.value = e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <Button class="absolute top-1 right-1" submit>
+      <Button class="absolute bottom-1 right-1" style="height: 28px" submit>
         Send
       </Button>
     </Form>
   )
 }
+
+// this will render text value into an invisible pseudo element, which will
+// then be used to calculate the height of the textarea
+const autoGrow = css`
+  &:after {
+    visibility: hidden;
+    content: attr(data-value) " ";
+    white-space: pre-wrap;
+  }
+`
