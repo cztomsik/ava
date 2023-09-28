af1009995c7e02946c872b7ba55d74131206bbf3
Kamil Tomsik <info@tomsik.cz>
2023-09-28 20:58:40 +0200

    forward props in Page.Header

--- src/app/_components/Page.tsx
+++ src/app/_components/Page.tsx
@@ -1,8 +1,8 @@
 const Page = ({ children }) => <>{children}</>
 
-const PageHeader = ({ title, children = null }) => {
+const PageHeader = ({ title, children = null, ...props }) => {
   return (
-    <header class="flex(& wrap) gap-2 items-end px-3 py-2 lg:pl-6 border(b-1 neutral-6)" data-drag-window>
+    <header class="flex(& wrap) gap-2 items-end px-3 py-2 lg:pl-6 border(b-1 neutral-6)" data-drag-window {...props}>
       <h2 class="mt-2 mr-auto text(xl primary-12) font-medium">{title}</h2>
 
       {children}
