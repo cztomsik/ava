64ff13e2907d87b7a11c04846da7470c99996a86
Kamil Tomsik <info@tomsik.cz>
2023-10-03 10:03:54 +0200

    update to ES2021

--- src/app/custom.d.ts
+++ src/app/custom.d.ts
@@ -1,11 +1,3 @@
-interface WeakRef<T> {
-  deref(): T | undefined
-}
-
-declare var WeakRef: {
-  new <T extends object>(target: T): WeakRef<T>
-}
-
 // so we don't need to typecast event.target every time
 interface EventTarget {
   parentElement: HTMLElement | null
