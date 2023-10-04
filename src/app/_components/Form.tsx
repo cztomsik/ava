2a1e8de54bae591f570a5f5de478d5796b6e408c
Kamil Tomsik <info@tomsik.cz>
2023-10-04 10:14:15 +0200

    refactor <Form, fix stale onSubmit

--- src/app/_components/Form.tsx
+++ src/app/_components/Form.tsx
@@ -1,19 +1,19 @@
-import { useCallback, useEffect, useRef } from "preact/hooks"
+import { useEffect, useRef } from "preact/hooks"
 
 export const Form = ({ onSubmit, ...props }) => {
-  const ref = useRef(null)
+  const ref = useRef<HTMLFormElement>(null)
 
-  // Focus the first element with autofocus or the first input
-  // TODO: maybe <select> should be here too but it doesn't feel right
   useEffect(() => {
-    const input = ref.current?.querySelector("[autofocus]") ?? ref.current?.querySelector("input, textarea")
+    const form = ref.current!
+
+    // prevent default form submission
+    form.addEventListener("submit", e => e.preventDefault())
+
+    // Focus the first element with autofocus or the first input
+    // TODO: maybe <select> should be here too but it doesn't feel right
+    const input: HTMLElement = form.querySelector("[autofocus]") ?? form.querySelector("input, textarea")
     input?.focus()
   }, [])
 
-  const handleSubmit = useCallback(event => {
-    event.preventDefault()
-    onSubmit()
-  }, [])
-
-  return <form noValidate ref={ref} onSubmit={handleSubmit} {...props} />
+  return <form noValidate ref={ref} onSubmit={onSubmit} {...props} />
 }
