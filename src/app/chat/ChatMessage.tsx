f6ed5bbcb22809c7dca7d6333bfd469f2bef8f53
Kamil Tomsik <info@tomsik.cz>
2023-10-04 10:20:26 +0200

    add message editing & deletion

--- src/app/chat/ChatMessage.tsx
+++ src/app/chat/ChatMessage.tsx
@@ -1,14 +1,58 @@
-import { Markdown } from "../_components"
+import { useEffect } from "preact/hooks"
+import { useSignal } from "@preact/signals"
+import { Button, Form, Markdown } from "../_components"
+import { useApi, useConfirm } from "../_hooks"
 
-// TODO: onEdit, onDelete
-export const ChatMessage = ({ id, role, content }) => (
-  <div class={`hstack px-4 py-6 odd:(border(y neutral-6) bg-neutral-1)`}>
-    <Avatar class="mr-4" role={role} />
-    <Markdown class="flex-1" input={"" + content} />
-  </div>
-)
+export const ChatMessage = ({ id, chat_id, role, content }) => {
+  const isEditing = useSignal(false)
 
-export const Avatar = ({ role, class: className = "" }) => {
+  return (
+    <div class={`hstack px-4 py-6 odd:(border(y neutral-6) bg-neutral-1)`} onDblClick={() => (isEditing.value = true)}>
+      <Avatar class="mr-4" role={role} />
+      {isEditing.value ? (
+        <EditMessage id={id} chat_id={chat_id} onClose={() => (isEditing.value = false)} />
+      ) : (
+        <Markdown class="flex-1" input={"" + content} />
+      )}
+    </div>
+  )
+}
+
+const EditMessage = ({ id, chat_id, onClose }) => {
+  const { data, put, del } = useApi(`chat/${chat_id}/messages/${id}`)
+
+  const content = useSignal("")
+  // TODO: useForm() or something
+  useEffect(() => {
+    if (data) {
+      content.value = data.content
+    }
+  }, [data])
+
+  const handleSubmit = async () => {
+    await put({ ...data, content: content.value })
+    onClose()
+  }
+
+  const handleDelete = useConfirm("Are you sure you want to delete this message?", del, [])
+
+  return (
+    <Form class="flex-1 vstack gap-2" onSubmit={handleSubmit}>
+      <textarea value={content} onInput={e => (content.value = e.target.value)} rows={5} autofocus />
+
+      <div class="hstack gap-2">
+        <Button submit>Save</Button>
+        <Button onClick={onClose}>Cancel</Button>
+
+        <a class="ml-auto p-1.5 text-red-11" onClick={handleDelete}>
+          Delete
+        </a>
+      </div>
+    </Form>
+  )
+}
+
+const Avatar = ({ role, class: className = "" }) => {
   const src = role === "user" ? userImg : assistantImg
 
   return <img class={`w-9 h-9 rounded self-start ${className}`} alt={role} src={src} />
