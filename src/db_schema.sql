e9022de127d95e7b76bc571bb91e0bb95dfec55f
Kamil Tomsik <info@tomsik.cz>
2023-10-05 11:51:25 +0200

    schema: add Chat.system_prompt

--- src/db_schema.sql
+++ src/db_schema.sql
@@ -1,15 +1,20 @@
-CREATE TABLE Prompt (
+-- 
+-- IMPORTANT:
+-- Table names need to be quoted in order to work with our migration tool.
+--
+CREATE TABLE "Prompt" (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   prompt TEXT NOT NULL
 ) STRICT;
 
-CREATE TABLE Chat (
+CREATE TABLE "Chat" (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
+  name TEXT NOT NULL,
+  system_prompt TEXT
 ) STRICT;
 
-CREATE TABLE ChatMessage (
+CREATE TABLE "ChatMessage" (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   chat_id INTEGER NOT NULL,
   role TEXT NOT NULL,
