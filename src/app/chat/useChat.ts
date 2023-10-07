50799ba03361d29288aae9fa9344c2646965eca0
Kamil Tomsik <info@tomsik.cz>
2023-10-07 17:16:13 +0200

    auto-create chats again

--- src/app/chat/useChat.ts
+++ src/app/chat/useChat.ts
@@ -1,21 +1,23 @@
 import { createContext } from "preact"
 import { useContext, useMemo } from "preact/hooks"
-import { useApi, useGenerate, useLocalStorage } from "../_hooks"
-import { signal } from "@preact/signals"
+import { signal, useSignal } from "@preact/signals"
+import { getApiContext, useApi, useGenerate, useLocalStorage } from "../_hooks"
+import { router } from "../router"
 
 export const ChatContext = createContext<ReturnType<typeof useChat>>(null as any)
 
 export const useChatContext = () => useContext(ChatContext)
 
 export const useChat = id => {
-  const chat = useApi(id && `chat/${id}`)
-  const messages = useApi(id && `chat/${id}/messages`)
+  // These are intentionally out of the useMemo
+  let chat = useApi(id && `chat/${id}`)
+  let messages = useApi(id && `chat/${id}/messages`)
   const input = useLocalStorage(`chat.${id}.input`, "")
   const { generate, result, ...progress } = useGenerate()
+  const generating = useSignal<any>(null)
 
   const ctx = useMemo(() => {
     const editing = signal<any>(null)
-    const generating = signal<any>(null)
 
     return {
       input,
@@ -58,8 +60,16 @@ export const useChat = id => {
         input.value = ""
 
         if (!id) {
-          alert("TODO")
-          return
+          const { id } = await getApiContext("chat").post({
+            name: `New chat ${new Date().toLocaleDateString()}`,
+            system_prompt: ctx.system_prompt,
+          })
+
+          // This will trigger a re-render, so everything below will happen in
+          // the "background" but it should still work, because we share the
+          // `generating` signal and the underlying api contexts.
+          router.navigate(`/chat/${id}`, true)
+          messages = getApiContext(`chat/${id}/messages`)
         }
 
         await messages.post({ role: "user", content })
