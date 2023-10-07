f4e9a9cb4973dd7cdae512bdb04662d1db9c1269
Kamil Tomsik <info@tomsik.cz>
2023-10-07 15:05:46 +0200

    small steps

--- src/app/chat/useChat.ts
+++ src/app/chat/useChat.ts
@@ -25,6 +25,10 @@ export const useChat = id => {
         return chat.data
       },
 
+      get system_prompt() {
+        return chat.data?.system_prompt ?? defaultPrompt
+      },
+
       get messages() {
         return messages.data
       },
@@ -57,11 +61,11 @@ export const useChat = id => {
         await messages.post({ role: "user", content })
         await messages.post({ role: "assistant", content: "" })
 
-        await ctx.generateMessage(messages.data[messages.data.length - 1])
+        await ctx.generateMessage(messages.data![messages.data!.length - 1])
       },
 
       async undo() {
-        if (messages.data.length > 0) {
+        if (messages.data && messages.data.length > 0) {
           const lastIndex = messages.data.findLastIndex(m => m.role === "user") ?? 0
           input.value = messages.data[lastIndex].content
 
@@ -82,8 +86,9 @@ export const useChat = id => {
 
       async generateMessage(message: any, startWith = "") {
         try {
-          const index = messages.data.indexOf(message)
-          const prompt = serializePrompt([...messages.data.slice(0, index), { ...message, content: startWith }])
+          const index = messages.data!.indexOf(message)
+          const prompt =
+            ctx.system_prompt + serializePrompt([...messages.data!.slice(0, index), { ...message, content: startWith }])
 
           generating.value = message
           editing.value = null
@@ -112,6 +117,5 @@ export const useChat = id => {
 
 const defaultPrompt =
   "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n\n"
-
 const serializePrompt = messages =>
-  defaultPrompt + messages.reduce((res, m) => res + `${m.role.toUpperCase()}: ${m.content}\n`, "").trimEnd()
+  messages.reduce((res, m) => res + `${m.role.toUpperCase()}: ${m.content}\n`, "").trimEnd()
