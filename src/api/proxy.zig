1de6ca5c12f0410113c011f2fe53af5680898a19
Kamil Tomsik <info@tomsik.cz>
2023-10-25 13:57:40 +0200

    fix proxy

--- src/api/proxy.zig
+++ src/api/proxy.zig
@@ -4,9 +4,10 @@ const server = @import("../server.zig");
 pub fn @"POST /proxy"(ctx: *server.Context) !void {
     const url = try ctx.readJson([]const u8);
 
-    inline for (.{ "Content-Type", "Content-Length", "Host", "Referer", "Origin" }) |h| {
+    inline for (.{ "Accept-Encoding", "Content-Type", "Content-Length", "Host", "Referer", "Origin" }) |h| {
         _ = ctx.res.request.headers.delete(h);
     }
+    try ctx.res.request.headers.append("Accept-Encoding", "gzip, deflate");
 
     var client: std.http.Client = .{ .allocator = ctx.arena };
     defer client.deinit();
@@ -23,6 +24,7 @@ pub fn @"POST /proxy"(ctx: *server.Context) !void {
     _ = ctx.res.headers.delete("Transfer-Encoding");
     _ = ctx.res.headers.delete("Content-Encoding");
     _ = ctx.res.headers.delete("Content-Length");
+    _ = ctx.res.headers.delete("Link"); // Otherwise browser will try to preload non-existent resources
     try ctx.res.send();
 
     var buf: [512]u8 = undefined;
