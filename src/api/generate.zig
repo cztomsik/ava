99133935ccfda466960bd7271c56baaa6a1ddf1c
Kamil Tomsik <info@tomsik.cz>
2023-10-26 12:28:52 +0200

    move max_tokens & trim_first to server api

--- src/api/generate.zig
+++ src/api/generate.zig
@@ -3,13 +3,15 @@ const server = @import("../server.zig");
 const db = @import("../db.zig");
 const llama = @import("../llama.zig");
 
-pub fn @"POST /generate"(ctx: *server.Context) !void {
-    const params = try ctx.readJson(struct {
-        model_id: u32,
-        prompt: []const u8,
-        sampling: llama.SamplingParams = .{},
-    });
+const GenerateParams = struct {
+    model_id: u32,
+    prompt: []const u8,
+    max_tokens: u32 = 2048,
+    trim_first: bool = false,
+    sampling: llama.SamplingParams = .{},
+};
 
+pub fn @"POST /generate"(ctx: *server.Context, params: GenerateParams) !void {
     try ctx.sendJson(.{ .status = "Waiting for the model..." });
     var model_path = try db.getString(ctx.arena, "SELECT path FROM Model WHERE id = ?", .{params.model_id});
     var cx = try llama.Pool.get(model_path, 60_000);
@@ -23,7 +25,14 @@ pub fn @"POST /generate"(ctx: *server.Context) !void {
         _ = try cx.evalOnce();
     }
 
+    var tokens: u32 = 0;
+
     while (try cx.generate(&params.sampling)) |content| {
-        try ctx.sendJson(.{ .content = content });
+        try ctx.sendJson(.{
+            .content = if (tokens == 0 and params.trim_first) std.mem.trimLeft(u8, content, " \t\n\r") else content,
+        });
+
+        tokens += 1;
+        if (tokens >= params.max_tokens) break;
     }
 }
