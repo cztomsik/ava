145fd3ea7dd1971f25af57726bbdfc411bbee74a
Kamil Tomsik <info@tomsik.cz>
2023-10-27 14:12:35 +0200

    update empty instructions

--- src/app/chat/NoMessages.tsx
+++ src/app/chat/NoMessages.tsx
@@ -4,8 +4,16 @@ export const NoMessages = () => (
     <ul class="list-disc mt-2 ml-4">
       <li>Select model in the bottom left corner.</li>
       <li>Use the input below to start chatting.</li>
-      <li>Use the dropdown above to load a previous chat.</li>
+      <li>
+        Use the list on the left to load a previous chat.
+        <br />
+        (hidden if the window size is too small)
+      </li>
       <li>You can type multi-line messages with Shift+Enter.</li>
+      <li>
+        Double click on a message to edit it, or for <strong>partial completion</strong>.
+      </li>
+      <li>You can also change the system-prompt at the top by double clicking it.</li>
     </ul>
   </div>
 )
