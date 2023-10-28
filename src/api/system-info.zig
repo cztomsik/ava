d7d6da9dc4de45fe61e1383de11b2ac3e5b52d3c
Kamil Tomsik <info@tomsik.cz>
2023-10-28 11:12:46 +0200

    move getOsVersion() to system-info

--- src/api/system-info.zig
+++ src/api/system-info.zig
@@ -1,7 +1,6 @@
 const builtin = @import("builtin");
 const std = @import("std");
 const server = @import("../server.zig");
-const util = @import("../util.zig");
 
 pub fn @"GET /system-info"(ctx: *server.Context) !void {
     const user_home = try std.process.getEnvVarOwned(ctx.arena, if (builtin.target.os.tag == .windows) "USERPROFILE" else "HOME");
@@ -9,11 +8,40 @@ pub fn @"GET /system-info"(ctx: *server.Context) !void {
 
     return ctx.sendJson(.{
         .os = builtin.os.tag,
-        .os_version = try util.getOsVersion(ctx.arena),
+        .os_version = try getOsVersion(ctx.arena),
         .arch = builtin.cpu.arch,
         .cpu_count = std.Thread.getCpuCount() catch 0,
         .total_system_memory = std.process.totalSystemMemory() catch 0,
         .user_home = user_home,
         .user_downloads = user_downloads,
     });
+}
+
+fn getOsVersion(allocator: std.mem.Allocator) ![]const u8 {
+    if (comptime builtin.os.tag == .macos) {
+        var f = try std.fs.openFileAbsolute("/System/Library/CoreServices/.SystemVersionPlatform.plist", .{});
+        defer f.close();
+
+        var contents = try f.readToEndAlloc(allocator, 1024);
+        defer allocator.free(contents);
+
+        if (std.mem.indexOf(u8, contents, "ProductVersion")) |a| {
+            if (std.mem.indexOf(u8, contents[a..], "<string>")) |b| {
+                if (std.mem.indexOf(u8, contents[a + b ..], "</string>")) |c| {
+                    return allocator.dupe(u8, contents[a + b + 8 .. a + b + c]);
+                }
+            }
+        }
+    }
+
+    if (comptime builtin.os.tag == .windows) {
+        var info: std.os.windows.RTL_OSVERSIONINFOW = undefined;
+        info.dwOSVersionInfoSize = @sizeOf(@TypeOf(info));
+
+        if (std.os.windows.ntdll.RtlGetVersion(&info) == .SUCCESS) {
+            return std.fmt.allocPrint(allocator, "{d}.{d}", .{ info.dwMajorVersion, info.dwMinorVersion });
+        }
+    }
+
+    return "unknown";
 }
