069acfb32d3a0da1e988483614f55edd30557d91
Kamil Tomsik <info@tomsik.cz>
2023-10-31 00:13:38 +0100

    replace ring with outline

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -27,7 +27,11 @@ const globals = () => ({
     select,
     textarea,
     .form-control {
-      @apply inline-block appearance-none outline-none text(ellipsis neutral-12) px-2 py-[5px] bg-neutral-1 border(1 neutral-8 focus:transparent) focus:ring(& primary-10) rounded-md resize-none;
+      @apply inline-block appearance-none text(ellipsis neutral-12) px-2 py-[5px] bg-neutral-1 border(1 neutral-8) rounded-md resize-none;
+    }
+
+    :focus {
+      @apply border-transparent outline(& [3px] primary-7);
     }
   `,
 })
