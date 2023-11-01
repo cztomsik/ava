37e57d5571ab6726f3934cd2f879bd35a9bda739
Kamil Tomsik <info@tomsik.cz>
2023-11-01 17:57:39 +0100

    WIP: useWorkflow()

--- src/app/workflow/Workflow.tsx
+++ src/app/workflow/Workflow.tsx
@@ -1,60 +1,62 @@
 import { Clock, Cloud, MousePointerSquare, Play, Repeat2, TableProperties, Wand2 } from "lucide"
-import { AutoScroll, Icon, IconButton, Page } from "../_components"
+import { Icon, IconButton, Page, TableInput } from "../_components"
 import { humanDuration } from "../_util"
-import { examples } from "./_examples"
-import { runLog, runWorkflow } from "./runner"
+import { useAriaList } from "../_hooks"
+import { WorkflowContext, useWorkflow, useWorkflowContext } from "./useWorkflow"
+import { data } from "./_examples"
 
 export const Workflow = ({ params }) => {
-  const id = +params.id
-  const workflow = examples.find(w => w.id === id)
-
-  const handleRun = () => {
-    runWorkflow(id)
-  }
+  const workflow = useWorkflow(+params.id)
+  const list = useAriaList()
 
   return (
-    <Page>
-      <Page.Header title={workflow?.name}>
-        <IconButton title="Run" icon={Play} onClick={handleRun} />
-      </Page.Header>
+    <WorkflowContext.Provider value={workflow}>
+      <Page>
+        <Page.Header title={workflow.data.name}>
+          <IconButton title="Run" icon={Play} onClick={workflow.runAll} />
+        </Page.Header>
 
-      <div class="flex overflow-hidden">
-        <div class="flex-1 vstack items-center bg-gray-2 py-10 overflow-auto">
-          <Steps steps={workflow?.steps} />
-        </div>
-
-        <div class="p-4 border(l-2 gray-7) w-[400px] overflow-y-auto">
-          <h3 class="text-gray-10 text-sm uppercase font-medium">Run Log</h3>
-
-          <div class="mt-4 vstack divide-y divide-gray-7 whitespace-pre-wrap break-all">
-            {runLog.value.length === 0 && <div class="text-gray-11 py-3">Progress will be shown here.</div>}
-
-            {runLog.value.map(l => (
-              <div class="text-gray-11 py-3">{l}</div>
-            ))}
+        <div class="flex overflow-hidden">
+          <div class="flex-1 vstack items-center bg-gray-2 py-10 overflow-auto" {...list}>
+            <Steps steps={workflow.data.steps} />
           </div>
 
-          <AutoScroll />
+          <div class="p-4 border(l-2 gray-7) w-[600px] overflow-y-auto">
+            <h3 class="text-gray-10 text-sm uppercase font-medium">Run Log</h3>
+
+            <TableInput value={data} />
+          </div>
         </div>
-      </div>
-    </Page>
+      </Page>
+    </WorkflowContext.Provider>
   )
 }
 
-const Steps = ({ class: className = "", steps }) => (
-  <div class={`vstack items-start ${className}`}>
-    {steps.map(s => (
-      <Step {...renderers[Object.keys(s)[0]](Object.values(s)[0])} />
-    ))}
-  </div>
-)
+const Steps = ({ class: className = "", steps }) => {
+  const workflow = useWorkflowContext()
 
-const Step = ({ icon, title, subtitle = "", selected = false, children = null as any }) => (
+  return (
+    <div class={`vstack items-start ${className}`}>
+      {steps.map(s => (
+        <Step
+          selected={workflow.selectedStep === s}
+          onSelect={() => workflow.selectStep(s)}
+          {...renderers[Object.keys(s)[0]](Object.values(s)[0])}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Step = ({ icon, title, subtitle = "", children = null as any, selected, onSelect }) => (
   <>
     <div
+      role="listitem"
+      onFocus={onSelect}
       class={`hstack gap-3 w-72 px-4 py-2 bg-gray-1 rounded-md border-2 ${
         selected ? "border-blue-8 ring(& blue-5)" : "border-gray-7"
       }`}
+      {...(selected ? { tabIndex: 0, "aria-selected": true } : { tabIndex: -1 })}
     >
       <div class={`w-10 h-10 rounded-full flex items-center justify-center ${selected ? "bg-blue-5" : "bg-gray-3"}`}>
         <Icon icon={icon} />
--- src/app/workflow/runner.ts
+++ src/app/workflow/runner.ts
@@ -1,15 +1,11 @@
 import { signal } from "@preact/signals"
 import { generate } from "../_hooks/useGenerate"
-import { humanDuration, parseHTML } from "../_util"
+import { parseHTML } from "../_util"
 import { examples } from "./_examples"
-
-export const runLog = signal([] as any[])
-const log = item => (runLog.value = [...runLog.value, item])
 
 // TODO: this should be implemented on the server side
 const handlers = {
   wait: ({ duration }) => {
-    log(`Wait for ${humanDuration(duration)}`)
     return new Promise(resolve => setTimeout(resolve, duration * 1000))
   },
 
@@ -17,26 +13,18 @@ const handlers = {
     const result = signal("")
     const status = signal(null)
 
-    log(`Generate "${input.slice(0, 100)}..."`)
-    log(status)
-    log(result)
     return generate({ ...opts, prompt: input }, result, status)
   },
 
   instruction: ({ instruction, ...opts }, input) => {
-    log(`Instruction "${instruction.slice(0, 100)}"`)
-    log(`Input "${input.slice(0, 100)}"`)
     return handlers.generate(opts, `ASSISTANT: ${input}\n\nUSER: ${instruction}\n\nASSISTANT: Sure!`)
   },
 
   http_request: ({ url }) => {
-    log(`HTTP request ${url}`)
     return fetch("/api/proxy", { method: "POST", body: JSON.stringify(url) }).then(res => res.text())
   },
 
   query_selector: ({ selector, limit = 2, clean = true }, input) => {
-    log(`Query selector ${selector} on ${input.slice(0, 100)}...`)
-
     return [...parseHTML(input, clean).querySelectorAll(selector)]
       .slice(0, limit)
       .map(el => el.innerHTML)
@@ -51,20 +39,16 @@ export const runWorkflow = async (id: number) => {
     throw new Error(`Workflow with id ${id} not found`)
   }
 
-  runLog.value = ["Running workflow " + id]
-
   const runStep = async (step, input) => {
     for (const k in handlers) {
       if (k in step) {
-        console.log("Running step", step, input)
         const res = await handlers[k](step[k], input)
-        console.log("Step result", res)
 
         return res
       }
     }
 
-    alert("TODO: implement step" + JSON.stringify(step))
+    throw new Error(`No handler found for step ${JSON.stringify(step)}`)
   }
 
   let input = null
