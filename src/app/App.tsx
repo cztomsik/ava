90c4572b769d025634f99d5a92e204ab7e474feb
Kamil Tomsik <info@tomsik.cz>
2023-11-10 20:04:56 +0100

    inline <Layout into <App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,27 @@
-import { ErrorBoundary, Layout, ModalBackdrop } from "./_components"
+import { ErrorBoundary, ModalBackdrop } from "./_components"
 import { Sidebar } from "./Sidebar"
 import { router } from "./router"
+
+import { css } from "@twind/core"
+
+export const grid = css`
+  @apply grid h-screen;
+
+  grid-template-columns: min-content min-content 1fr min-content;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "sidebar list header header"
+    "sidebar list main details"
+    "sidebar list footer footer";
+`
 
 export const App = () => {
   return (
     <ErrorBoundary class="text(base neutral-12)">
-      <Layout class="h-screen">
+      <div class={grid}>
         <Sidebar />
         <router.currentRoute.component params={router.params} />
-      </Layout>
+      </div>
 
       <ModalBackdrop />
     </ErrorBoundary>
