22733a2e5d74926d27436476a75069490ab85d30
Kamil Tomsik <info@tomsik.cz>
2023-11-10 20:40:22 +0100

    support null key in useLocalStorage()

--- src/app/_hooks/useLocalStorage.ts
+++ src/app/_hooks/useLocalStorage.ts
@@ -1,12 +1,17 @@
 import { useMemo } from "preact/hooks"
 import { Signal, effect } from "@preact/signals"
 
-export const useLocalStorage = <V>(key: string, initialValue: V) => {
+/**
+ * Returns a Signal that is synced with localStorage, unless the key is null.
+ */
+export const useLocalStorage = <V>(key: string | null, initialValue: V) => {
   return useMemo(() => {
-    const signal = new Signal<V>(JSON.parse(localStorage.getItem(key) as any) ?? initialValue)
+    const signal = new Signal<V>(key ? JSON.parse(localStorage.getItem(key) as any) ?? initialValue : initialValue)
 
     effect(() => {
-      localStorage.setItem(key, JSON.stringify(signal.value))
+      if (key) {
+        localStorage.setItem(key, JSON.stringify(signal.value))
+      }
     })
 
     return signal
