ec80094de6e886396b1ca5f414fa92ffc3287fd9
Kamil Tomsik <info@tomsik.cz>
2023-11-13 16:22:58 +0100

    remove twind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,23 +2,20 @@ import { ErrorBoundary, ModalBackdrop } from "./_components"
 import { Sidebar } from "./Sidebar"
 import { router } from "./router"
 
-import { css } from "@twind/core"
-
-export const grid = css`
-  @apply grid h-screen;
-
-  grid-template-columns: min-content min-content 1fr min-content;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas:
+const grid = {
+  gridTemplateColumns: "min-content min-content 1fr min-content",
+  gridTemplateRows: "auto 1fr auto",
+  gridTemplateAreas: `
     "sidebar list header header"
     "sidebar list main details"
-    "sidebar list footer footer";
-`
+    "sidebar list footer footer"
+  `,
+}
 
 export const App = () => {
   return (
     <ErrorBoundary class="text(base neutral-12)">
-      <div class={grid}>
+      <div class="grid h-screen" style={grid}>
         <Sidebar />
         <router.currentRoute.component params={router.params} />
       </div>
