44662184ff04feff828807ffe1285f722da1a11e
Kamil Tomsik <info@tomsik.cz>
2023-11-13 19:31:16 +0100

    tw: more props/shorthands, expand &

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -17,14 +17,18 @@ export const colors = {
 export const theme = {
   color: (k, v) => colors[k]?.[v] ?? k,
   space: (v, p = "w") => ({ auto: "auto", px: "1px", full: "100%", screen: `100v${p}` }[v] ?? `${v * 0.25}rem`),
-  fontSize: v => ({ xs: "12px", sm: "13px", base: "14px", lg: "16px", xl: "18px", "2xl": "20px" }[v] ?? v),
-  fontWeight: v => ({ normal: 400, medium: 500, semibold: 600, bold: 700 }[v] ?? v),
+  fontSize: v => ({ xs: "12px", sm: "13px", base: "14px", lg: "16px", xl: "18px", "2xl": "20px" }[v]),
+  fontWeight: v => ({ medium: 500, semibold: 600 }[v] ?? v),
+  rounded: (v = "base") => ({ none: "0", sm: "2px", base: "4px", md: "6px", lg: "8px", full: "999px" }[v]),
 }
 
 type Shorthand = [RegExp, string | ((match) => string)]
-export const shorthands: Array<[RegExp, string | ((match) => string)]> = [
-  [/^hstack$/, ([_]) => `flex flex-row items-center`],
-  [/^vstack$/, ([_]) => `flex flex-col`],
+export const shorthands: Array<Shorthand> = [
+  [/^hstack$/, "flex flex-row items-center"],
+  [/^vstack$/, "flex flex-col"],
+  [/^row$/, "flex(& row) -mx-2"], // TODO: minus
+  [/^col$/, "flex-1 px-2"],
+  [/^truncate$/, ([_]) => `overflow-hidden text-overflow-ellipsis whitespace-nowrap`],
 ]
 
 type Rule = [RegExp, string | ((match) => string), string?]
@@ -35,31 +39,35 @@ export const rules: Rule[] = [
   [/^(uppercase|lowercase|capitalize|normal-case)$/, "text-transform"],
   [/^select-(.+)$/, "user-select"],
   [/^bg-(\w+)-(\d+)$/, "background-color", "color"],
+  [/^rounded(?:-(.+))?$/, "border-radius", "rounded"],
   [/^(border|outline)-(\w+)-(\d+)$/, ([_, p, k, d]) => `${p}-color: ${theme.color(k, d)}`],
   [/^[pm](\w)?-([\w\.]+)$/, ([m, e, v]) => repeat(m[0] === "p" ? "padding" : "margin", edges(e), theme.space(v))],
   [/^[wh]-([\w\.]+)$/, ([m, v]) => `${m[0] === "w" ? "width" : "height"}: ${theme.space(v, m[0])}`],
-  [/^(inline|block|inline-block|flex|grid|table|table-.*)$/, ([_, v]) => `display: ${v}`],
+  [/^(inline|block|inline-block|flex|grid|table|table-.*)$/, "display"],
   [/^grid-(cols|rows)-(\d+)$/, ([_, p, d]) => `grid-template-${p.replace(/col/, "column")}: repeat(${d},minmax(0,1fr))`],
-  [/^flex-(row|col)$/, ([_, v]) => `flex-direction: ${v.replace(/col/, "column")}`],
+  [/^flex-((row|col)(-reverse)?)$/, ([_, v]) => `flex-direction: ${v.replace(/col/, "column")}`],
+  [/^flex-(.+)$/, "flex"],
   [/^gap-(.+)$/, "gap", "space"],
   [/^items-(.+)$/, "align-items"],
   [/^justify-(.+)$/, "justify-content"],
   [/^self-(.+)$/, "align-self"],
-  [/^(relative|absolute|fixed)$/, "position"],
+  [/^whitespace-(.+)$/, "white-space"],
+  [/^(static|relative|absolute|fixed|sticky)$/, "position"],
   [/^(?:inset-?([xy])?|top|right|left|bottom)-(.+)$/, ([m, e = m[0], v]) => repeat("", edges(e), theme.space(v), "inset")],
-  [/^(cursor|fill|opacity|(?:overflow(?:-[xy])?)|pointer-events|transition)-(.+)$/, ([_, p, v]) => `${p}: ${v}`],
+  // prettier-ignore
+  [/^(cursor|fill|opacity|(?:overflow(?:-[xy])?)|pointer-events|text-overflow|transition)-(.+)$/, ([_, p, v]) => `${p}: ${v}`],
   [/^\[([\w-]+):(.+)]$/, ([_, p, v]) => `${p}: ${v}`],
 ]
 
 export const repeat = (prefix, parts, value, shorthand = prefix) =>
-  parts?.map(p => `${prefix}${prefix && "-"}${p}: ${value}`).join("; ") ?? `${shorthand}: ${value}`
+  parts?.map(p => `${prefix}${prefix && "-"}${p}: ${value}`).join(";\n") ?? `${shorthand}: ${value}\n`
 
 export const edges = ch =>
   ({ t: ["top"], r: ["right"], b: ["bottom"], l: ["left"], x: ["left", "right"], y: ["top", "bottom"] }[ch])
 
-const [l1, l2] = Array.from(Array(2), () => document.head.appendChild(document.createElement("style")).sheet!)
-const cache = Object.create(null)
-const atoms = new Set()
+const layers = Array.from(Array(3), () => document.head.appendChild(document.createElement("style")).sheet!)
+const atoms = new Map<string, string>()
+const push = (layer, atom, body) => (layers[layer].insertRule(`.${escape(atom)} { ${body} }`), atoms.set(atom, body))
 
 export const compile = (str: string) => {
   let m
@@ -68,21 +76,28 @@ export const compile = (str: string) => {
   outer: for (let atom of className.split(/\s+/g)) {
     if (atoms.has(atom)) continue
 
+    for (const [regex, resolve] of shorthands) {
+      if ((m = atom.match(regex))) {
+        const body = compile(typeof resolve === "string" ? resolve : resolve(m))
+          .split(/\s+/)
+          .map(a => atoms.get(a))
+          .join("; ")
+        push(0, atom, body)
+        continue outer
+      }
+    }
+
     for (const [regex, resolve, themeFn] of rules) {
       if ((m = atom.match(regex))) {
-        // console.log(`CSS: .${escape(atom)} { ${resolve(m)} }`)
-        l1.insertRule(
-          `.${escape(atom)} { ${
-            typeof resolve === "string" ? `${resolve}: ${themeFn ? theme[themeFn](...m.slice(1)) : m[1]}` : resolve(m)
-          } }`
-        )
-        atoms.add(atom)
+        const body =
+          typeof resolve === "string" ? `${resolve}: ${themeFn ? theme[themeFn](...m.slice(1)) : m[1]}` : resolve(m)
+        push(body.includes("\n") ? 1 : 2, atom, body)
         continue outer
       }
     }
 
     console.log("unknown", atom)
-    atoms.add(atom)
+    atoms.set(atom, "")
   }
 
   return className
@@ -95,7 +110,7 @@ export const expand = s => {
     s.replace(/([\w-]+)(:)?\(([^\(\)]+)\)/g, (_, prefix, join = "-", parts) =>
       parts
         .split(/\s+/g)
-        .map(part => prefix + join + part)
+        .map(part => (part === "&" ? prefix : prefix + join + part))
         .join(" ")
     )
 
@@ -110,8 +125,7 @@ const old = options.diffed
 options.diffed = (vnode: any) => {
   if (old) old(vnode)
   if (DEV && vnode.props?.className) console.error("className is not supported, use class", vnode.type, vnode.props)
-  if (typeof vnode.type === "string" && vnode.props?.class)
-    vnode.__e.className = cache[vnode.props.class] ?? (cache[vnode.props.class] = compile(vnode.props.class))
+  if (typeof vnode.type === "string" && vnode.props?.class) vnode.__e.className = compile(vnode.props.class)
 }
 
 // prettier-ignore
