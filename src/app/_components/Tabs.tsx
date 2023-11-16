23c53a4391e227d63addb0a40c5791bf53daf60b
Kamil Tomsik <info@tomsik.cz>
2023-11-16 15:11:41 +0100

    fix Tabs

--- src/app/_components/Tabs.tsx
+++ src/app/_components/Tabs.tsx
@@ -1,7 +1,7 @@
 export const Tabs = ({ class: className = "", children }) => {
   return (
     <div class={`font-medium text(sm center neutral-9) ${className}`}>
-      <div class="flex flex-wrap [&>*]:(p-4 mb-px border(b-2 transparent) [&.active]:(text-blue-10 border-blue-10))">
+      <div class="flex flex-wrap children:(p-4 mb-px border(b-2 transparent)) children:aria-current:(text-blue-10 border-blue-10)">
         {children}
       </div>
     </div>
