f07f940596200c1e55deb29f3980163dfd75b474
Kamil Tomsik <info@tomsik.cz>
2023-11-16 15:10:45 +0100

    use border-1

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -3,7 +3,9 @@ import { Link } from "./Link"
 export const Button = ({ class: className = "", submit = false, primary = submit, text = false, ...props }) => {
   props.type = submit ? "submit" : "button"
 
-  props.class = `h-8 border rounded-md inline-flex items-center justify-center px-${text ? 1 : 3} text-sm bg-gradient-to-b ${
+  props.class = `h-8 border-1 rounded-md inline-flex items-center justify-center px-${
+    text ? 1 : 3
+  } text-sm bg-gradient-to-b ${
     text
       ? "border-transparent"
       : primary
