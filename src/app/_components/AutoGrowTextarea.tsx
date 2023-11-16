3c5bfbb82c6c4bf0e4f2ca171035f2c9e61b2cfa
Kamil Tomsik <info@tomsik.cz>
2023-11-16 14:21:32 +0100

    add .form-control to <AutoGrowTextarea

--- src/app/_components/AutoGrowTextarea.tsx
+++ src/app/_components/AutoGrowTextarea.tsx
@@ -1,6 +1,6 @@
 export const AutoGrowTextarea = ({ class: className = "", ...props }) => (
   <div class="vstack relative">
-    <textarea class={`!absolute !inset-0 ${className}`} {...props} />
+    <textarea class={`!absolute !inset-0 form-control ${className}`} {...props} />
     <div
       // render to invisible pseudo element, used to measure the height
       class={`form-control ${className} invisible whitespace-pre-wrap max-h-[30vh] overflow-y-hidden after:[content:attr(data-value)_"_"]`}
