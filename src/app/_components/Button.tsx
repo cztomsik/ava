ee7d44abe7016e371563ef85eefffd3f5b9d7e35
Kamil Tomsik <info@tomsik.cz>
2023-11-22 09:55:16 +0100

    fix colors in dark mode

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -3,15 +3,13 @@ import { Link } from "./Link"
 export const Button = ({ class: className = "", submit = false, primary = submit, text = false, ...props }) => {
   props.type = submit ? "submit" : "button"
 
-  props.class = `h-8 border-1 rounded-md inline-flex items-center justify-center px-${
-    text ? 1 : 3
-  } text-sm bg-gradient-to-b ${
+  props.class = `btn ${className} ${
     text
-      ? "border-transparent"
+      ? "px-1"
       : primary
-      ? "bg-primary(10 dark:11) from-primary(8 dark:9) text-sky(1 dark:4) border-primary(8 dark:10)"
-      : "bg-neutral-5 from(white dark:neutral-11) text-neutral-12 border-neutral-6 [box-shadow:0_1px_1px_0_rgba(0,0,0,0.3)]"
-  } ${className}`
+      ? "bg-primary-9 from-primary(8 dark:10) text-white border-primary-9"
+      : "bg-neutral-4 from-neutral(1 dark:8) text-neutral-12 border-neutral-6 [box-shadow:0_1px_1px_0_rgba(0,0,0,0.3)]"
+  }`
 
   return props.href ? <Link {...props} /> : <button {...props} />
 }
--- src/app/_components/List.tsx
+++ src/app/_components/List.tsx
@@ -20,7 +20,7 @@ const ListItem = ({ class: className = "", children, selected = false, ...props 
     <div
       role="listitem"
       tabIndex={-1}
-      class="group px-6 py-3 border(b-1 neutral-6) outline-none aria-selected:(bg-neutral-6 focus:(bg-primary-10 text-white))"
+      class="group px-6 py-3 border(b-1 neutral-6) outline-none aria-selected:(bg-neutral-6 focus:(bg-primary(10 dark:8) text-white))"
       {...props}
     >
       {children}
@@ -31,7 +31,7 @@ const ListItem = ({ class: className = "", children, selected = false, ...props 
 const ListItemTitle = ({ children }) => <h4 class="font-semibold truncate">{children}</h4>
 
 const ListItemSubtitle = ({ children }) => (
-  <p class="m-0 truncate text(sm neutral-11 group-focus:neutral(6 dark:6))">{children}</p>
+  <p class="m-0 truncate text(sm neutral-11 group-focus:neutral(6 dark:11))">{children}</p>
 )
 
 export { List }
