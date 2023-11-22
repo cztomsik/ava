2319a2bdcfc5a14594000a631f69e63a4573bac1
Kamil Tomsik <info@tomsik.cz>
2023-11-22 09:18:35 +0100

    remove <Grid

--- src/app/_components/FormGrid.tsx
+++ src/app/_components/FormGrid.tsx
@@ -1,6 +1,7 @@
-import { Grid } from "./Grid"
-
 export const FormGrid = ({ class: className = "", ...props }) => (
   // align-self wouldn't add margin when the label is on top
-  <div class={`grid grid-cols-2 mb-4 [&_label]:(mt-1.5 md:text-right) ${className}`} {...props} />
+  <div
+    class={`grid gap-4 md:[grid-template-columns:min-content_1fr] mb-4 [&_label]:(mt-1.5 md:text-right) ${className}`}
+    {...props}
+  />
 )
