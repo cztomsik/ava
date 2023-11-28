09b0b7247f47d1e47e9461b59799e2b0bd9f04e3
Kamil Tomsik <info@tomsik.cz>
2023-11-28 11:57:12 +0100

    use gridTemplate shorthand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,16 @@ import { ErrorBoundary, ModalBackdrop } from "./_components"
 import { Sidebar } from "./Sidebar"
 import { router } from "./router"
 
-const grid = {
-  gridTemplateColumns: "min-content min-content 1fr min-content",
-  gridTemplateRows: "auto 1fr auto",
-  gridTemplateAreas: `
-    "sidebar list header header"
-    "sidebar list main details"
-    "sidebar list footer footer"
-  `,
-}
+const gridTemplate = `
+  "sidebar list header header" auto
+  "sidebar list main details" 1fr
+  "sidebar list footer footer" auto / min-content min-content 1fr min-content
+`
 
 export const App = () => {
   return (
     <ErrorBoundary class="text(base neutral-12)">
-      <div class="grid h-screen" style={grid}>
+      <div class="grid h-screen" style={{ gridTemplate }}>
         <Sidebar />
         <router.currentRoute.component params={router.params} />
       </div>
