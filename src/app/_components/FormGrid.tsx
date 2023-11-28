4cb00c6a96e9c90d21285c25fdc99849cc753aca
Kamil Tomsik <info@tomsik.cz>
2023-11-28 12:49:52 +0100

    remove Grid, use inline grid-col-[xxx]

--- src/app/_components/FormGrid.tsx
+++ src/app/_components/FormGrid.tsx
@@ -1,6 +1,4 @@
-import { Grid } from "./Grid"
-
 export const FormGrid = ({ class: className = "", ...props }) => (
   // align-self wouldn't add margin when the label is on top
-  <Grid cols="auto 1fr" class={`mb-4 [&_label]:(mt-1.5 md:text-right) ${className}`} {...props} />
+  <div class={`grid grid-cols-[auto_1fr] gap-4 mb-4 [&_label]:(mt-1.5 md:text-right) ${className}`} {...props} />
 )
