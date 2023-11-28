c1269cf77061896a43a712a877605354472b1dac
Kamil Tomsik <info@tomsik.cz>
2023-11-28 12:20:51 +0100

    className -> class

--- src/app/settings/System.tsx
+++ src/app/settings/System.tsx
@@ -8,7 +8,7 @@ export const System = () => {
 
   return (
     <SettingsPage>
-      <h3 className="my-3 text-lg">System info</h3>
+      <h3 class="my-3 text-lg">System info</h3>
 
       <div class="grid grid-cols(3 lg:4 xl:5 2xl:6) gap-3 mb-4">
         <Value label="Operating system" value={`${system?.os} ${system?.os_version}`} />
@@ -17,7 +17,7 @@ export const System = () => {
         <Value label="Memory" value={system?.total_system_memory} />
       </div>
 
-      <h3 className="my-3 text-lg">Log</h3>
+      <h3 class="my-3 text-lg">Log</h3>
       <pre class="p-4 border(1 neutral-6) rounded-md overflow-auto shadow-inner">{log}</pre>
     </SettingsPage>
   )
