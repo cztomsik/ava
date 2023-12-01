dd59297e0fc1fd8f299aa813743b85cf643e8d27
Kamil Tomsik <info@tomsik.cz>
2023-12-01 21:20:12 +0100

    update model catalog

--- src/app/settings/ModelCatalog.tsx
+++ src/app/settings/ModelCatalog.tsx
@@ -55,19 +55,15 @@ const catalog = [
     size: 7865956224,
   },
   {
+    url: "https://huggingface.co/TheBloke/zephyr-7B-beta-GGUF/resolve/main/zephyr-7b-beta.Q4_K_M.gguf",
+    size: 4368438976,
+  },
+  {
     url: "https://huggingface.co/TheBloke/Airoboros-L2-7B-2.2-GGUF/resolve/main/airoboros-l2-7b-2.2.Q4_K_M.gguf",
     size: 4081004256,
   },
   {
-    url: "https://huggingface.co/Aryanne/Mamba-gpt-3B-v4-ggml-and-gguf/resolve/main/q4_0-gguf-mamba-gpt-3B_v4.gguf",
-    size: 1979924096,
-  },
-  {
-    url: "https://huggingface.co/Trelis/TinyLlama-1.1B-Chat-v0.1-GGUF/resolve/main/TinyLlama-1.1B-Chat-v0.1.Q4_K.gguf",
-    size: 667817216,
-  },
-  {
-    url: "https://huggingface.co/klosax/tinyllamas-stories-gguf/resolve/main/tinyllamas-stories-15m-f32.gguf",
-    size: 98229216,
+    url: "https://huggingface.co/TheBloke/rocket-3B-GGUF/resolve/main/rocket-3b.Q4_K_M.gguf",
+    size: 1708595136,
   },
 ]
