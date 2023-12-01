d8f06fa94e0ce34fd90b47c441a7d8d75ffb682f
Kamil Tomsik <info@tomsik.cz>
2023-12-01 18:57:02 +0100

    fix Table

--- src/app/_components/Table.tsx
+++ src/app/_components/Table.tsx
@@ -1,10 +1,7 @@
 export const Table = ({ class: className = "", ...props }) => (
-  <div class="flex">
+  <div>
     <div class={`rounded overflow-hidden border(1 neutral-7 b-0) ${className}`}>
-      <table
-        class="table-fixed border-collapse [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-3"
-        {...props}
-      />
+      <table class="table-fixed w-full [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-3" {...props} />
     </div>
   </div>
 )
