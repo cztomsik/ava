cd2d2c6e8f7756e6070a6bcafe4406c858f4c634
Kamil Tomsik <info@tomsik.cz>
2024-02-11 18:03:43 +0100

    Range: emit numbers

--- src/app/_components/Range.tsx
+++ src/app/_components/Range.tsx
@@ -1,11 +1,11 @@
-export const Range = ({ label, ...props }) => {
+export const Range = ({ label, onChange, ...props }) => {
   return (
     <div class="vstack gap-2">
       <label class="flex justify-between items-center">
         <span>{label}</span>
         <input type="number" {...props} />
       </label>
-      <input type="range" class="w-full" {...props} />
+      <input type="range" class="w-full" onChange={e => onChange(+e.target!.value)} {...props} />
     </div>
   )
 }
