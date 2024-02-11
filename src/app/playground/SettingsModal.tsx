186de1d3616e3854c908d6ab99f39116bc3134f3
Kamil Tomsik <info@tomsik.cz>
2024-02-11 18:04:32 +0100

    SettingsModal: remove unnecessary onChange

--- src/app/playground/SettingsModal.tsx
+++ src/app/playground/SettingsModal.tsx
@@ -15,19 +15,9 @@ export const SettingsModal = ({ sampleOptions, onClose, onSave }) => {
           <Field as={Range} name="presence_penalty" label="Presence penalty" min={0} max={2} step={0.01} />
           <Field as={Range} name="freq_penalty" label="Frequency penalty" min={0} max={2} step={0.01} />
 
-          <Field
-            as={Checkbox}
-            name="add_bos"
-            label="Add beginning of string token"
-            onChange={v => (sampleOptions.add_bos = v)}
-          />
-          <Field
-            as={Checkbox}
-            name="stop_eos"
-            label="Stop at end of string token"
-            onChange={v => (sampleOptions.stop_eos = v)}
-          />
-          <Field as={Checkbox} name="json" label="Sample JSON" onChange={v => (sampleOptions.json = v)} />
+          <Field as={Checkbox} name="add_bos" label="Add beginning of string token" />
+          <Field as={Checkbox} name="stop_eos" label="Stop at end of string token" />
+          <Field as={Checkbox} name="json" label="Sample JSON" />
         </FormGrid>
         <div class="gap-2 flex justify-end">
           <Button onClick={onClose} text>
