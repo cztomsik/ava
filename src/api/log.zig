2607e614e0bc0583f76e63ee011ee0ccd147889e
Kamil Tomsik <info@tomsik.cz>
2024-03-31 12:00:01 +0200

    fix log

--- src/api/log.zig
+++ src/api/log.zig
@@ -1,3 +1,7 @@
+const tk = @import("tokamak");
 const util = @import("../util.zig");
 
-pub const @"GET /log" = util.Logger.dump;
+pub fn @"GET /log"(ctx: *tk.Context) !void {
+    try ctx.res.setHeader("Content-Type", "text/plain");
+    try ctx.res.send(try util.Logger.dump(ctx.allocator));
+}
