9e4c0afa135adb22e5df69dd55cc799ef6f64370
Kamil Tomsik <info@tomsik.cz>
2024-03-31 12:57:49 +0200

    Table: lighten stripes

--- src/app/_components/Table.tsx
+++ src/app/_components/Table.tsx
@@ -1,5 +1,5 @@
 export const Table = ({ class: className = "", ...props }) => (
   <div class={`relative overflow-x-auto ${className}`}>
-    <table class="table-fixed w-full [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-3" {...props} />
+    <table class="table-fixed w-full [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-2" {...props} />
   </div>
 )
