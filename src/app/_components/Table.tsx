21c5aff41fa4e29d30fc98a5e5d08f370ba32713
Kamil Tomsik <info@tomsik.cz>
2024-03-31 12:38:44 +0200

    Table: remove border

--- src/app/_components/Table.tsx
+++ src/app/_components/Table.tsx
@@ -1,7 +1,5 @@
 export const Table = ({ class: className = "", ...props }) => (
-  <div>
-    <div class={`rounded overflow-hidden border(1 neutral-7 b-0) ${className}`}>
-      <table class="table-fixed w-full [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-3" {...props} />
-    </div>
+  <div class={`relative overflow-x-auto ${className}`}>
+    <table class="table-fixed w-full [&_th,&_td]:(p-2 border(b-1 neutral-7)) [&_tr]:even:bg-neutral-3" {...props} />
   </div>
 )
