cb11795dda51453b85ee7e632dcd7e9d4565b49c
Kamil Tomsik <info@tomsik.cz>
2024-04-19 19:08:36 +0200

    <Models: add model info modal

--- src/app/models/Models.tsx
+++ src/app/models/Models.tsx
@@ -1,12 +1,15 @@
-import { Button, Table, Modal, Page } from "../_components"
+import { Button, Table, Modal, Page, IconButton } from "../_components"
 import { ModelsPage } from "./ModelsPage"
 import { useQuery } from "../_hooks"
 import { fmtSize } from "../_util"
 import { ModelImporter } from "./ModelImporter"
 import { api } from "../api"
+import { Info } from "lucide"
 
 export const Models = () => {
   const { data: models = [] } = useQuery(api.listModels())
+
+  const handleInfo = id => Modal.open(ModelInfo, { id })
 
   const handleDelete = id => Modal.confirm("Are you sure you want to delete this model?").then(() => api.deleteModel(id))
 
@@ -20,6 +23,7 @@ export const Models = () => {
           <thead>
             <tr>
               <th>Model</th>
+              <th class="w-16"></th>
               <th class="w-24 text-right">Size</th>
               <th class="w-32"></th>
             </tr>
@@ -34,6 +38,9 @@ export const Models = () => {
             {models.map(m => (
               <tr>
                 <td class="capitalize">{m.name}</td>
+                <td class="text-right">
+                  <IconButton icon={Info} onClick={() => handleInfo(m.id)} />
+                </td>
                 <td class="text-right">{fmtSize(m.size)}</td>
                 <td class="text-center">
                   {m.imported ? (
@@ -48,5 +55,33 @@ export const Models = () => {
         </Table>
       </Page.Content>
     </ModelsPage>
+  )
+}
+
+const ModelInfo = ({ id, resolve }) => {
+  const { data: model = {}, loading: l1 } = useQuery(api.getModel(id))
+  const { data: meta = [], loading: l2 } = useQuery(api.getModelMeta(id))
+
+  return (
+    <Modal title={model.name ?? "Model info"} loading={l1 || l2} onClose={() => resolve()}>
+      <p class="text-neutral-11 -mt-2 mb-4">{model.path}</p>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {meta.map(({ key, value }) => (
+            <tr>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Modal>
   )
 }
