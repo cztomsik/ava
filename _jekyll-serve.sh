a632bd17f1ef587d5363dfd89293f19f3e01e55a
Kamil Tomsik <info@tomsik.cz>
2024-04-22 21:58:09 +0200

    docs: speedup jekyll script a bit?

--- _jekyll-serve.sh
+++ _jekyll-serve.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+rm -rf /tmp/lima/jekyll
+mkdir -p /tmp/lima/jekyll
+
+cat <<EOF > /tmp/lima/jekyll/Gemfile
+source "https://rubygems.org"
+gem "jekyll", "~> 3.9.5"
+gem "github-pages", group: :jekyll_plugins
+EOF
+
 lima nerdctl run --rm -it \
   -p 4000:4000 \
   -v $(pwd):/src \
   -v /tmp/lima/jekyll:/work \
   ruby:2.7-bullseye\
-  sh -c 'bundle init; \
-         bundle add jekyll --version "~> 3.9.5"; \
-         bundle add github-pages --group jekyll_plugins; \
-         bundle install && bundle exec jekyll serve -s /src --H=0.0.0.0 --force_polling'
+  sh -c 'cd /work; \
+         bundle install --jobs 8 && bundle exec jekyll serve -s /src --H=0.0.0.0 --force_polling'
