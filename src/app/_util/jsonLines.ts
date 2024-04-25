470e8aeef48aed1a843dacad743687253f0c9e79
Kamil Tomsik <info@tomsik.cz>
2024-04-25 21:39:08 +0200

    jsonLines: add sse=false flag

--- src/app/_util/jsonLines.ts
+++ src/app/_util/jsonLines.ts
@@ -1,13 +1,13 @@
-export async function* jsonLines(reader: ReadableStreamDefaultReader<Uint8Array>) {
+export async function* jsonLines(reader: ReadableStreamDefaultReader<Uint8Array>, sse = false) {
   let buf = ""
 
   for await (let chunk of chunks(reader)) {
     if (buf) chunk = buf + chunk
-    const lines = chunk.split("\n")
+    const lines = chunk.split(sse ? "\n\n" : "\n")
     buf = lines.pop()!
 
     for (const line of lines) {
-      if (line) yield JSON.parse(line)
+      if (line) yield JSON.parse(sse ? line.slice(6) : line) // skip "data: "
     }
   }
 }
