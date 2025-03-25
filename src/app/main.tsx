4322fa0c0c6b6eb3545e66693bf0c3d0c82f83e7
Kamil Tomsik <info@tomsik.cz>
2025-03-25 21:31:54 +0100

    macos: quickfix copy/paste in DEV

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,20 +5,17 @@ import { App } from "./App"
 
 render(<App />, document.querySelector("#app")!)
 
-// We only need this for macos because it's common to drag the window by
-// clicking on the app content.
-if ("webkit" in window) {
-  const delegate = e => {
-    if (e.target.matches("a, button, button *, input, select, textarea")) return
-    if (!e.target.closest("[data-drag-window]")) return
-
+// Quickfix for macos (should be DEV-only but we need to check)
+addEventListener("keypress", e => {
+  if (e.metaKey) {
+    if (e.key === "c") document.execCommand("copy")
+    else if (e.key === "v") document.execCommand("paste")
+    else if (e.key === "Z") document.execCommand("redo")
+    else if (e.key === "z") document.execCommand("undo")
+    else return
     e.preventDefault()
-    webkit.messageHandlers.event.postMessage(e.type)
   }
-
-  addEventListener("mousedown", delegate)
-  addEventListener("dblclick", delegate)
-}
+})
 
 if (!DEV) {
   addEventListener("contextmenu", e => {
