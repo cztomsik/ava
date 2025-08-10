464c1b7ac17827054159b72f70a1dc1a4e77b3c9
Kamil Tomsik <info@tomsik.cz>
2025-08-10 21:47:14 +0200

    chore: rename range->slider, add larger ring

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -1,7 +1,7 @@
 // TODO: until this gets fixed the :focus does not really work for ranges in Safari (and macos webview)
 //       https://bugs.webkit.org/show_bug.cgi?id=234516
 
-export const Range = ({ label, onChange, ...props }) => {
+export const Slider = ({ label, onChange, ...props }) => {
   return (
     <div class="vstack gap-2 mb-2">
       <label class="flex justify-between items-center">
@@ -10,7 +10,7 @@ export const Range = ({ label, onChange, ...props }) => {
       </label>
       <input
         type="range"
-        class="w-full appearance-none h-1 rounded-full bg-neutral-4 focus:outline-none thumb:appearance-none thumb:size-2 thumb:-mt-0.75 thumb:bg-neutral-1 thumb:ring thumb:ring-neutral-10 thumb:rounded-full focus:thumb:ring-primary-9"
+        class="w-full appearance-none h-1 rounded-full bg-neutral-4 focus:outline-none thumb:appearance-none thumb:size-2 thumb:-mt-0.75 thumb:bg-neutral-1 thumb:ring-2 thumb:ring-neutral-10 thumb:rounded-full focus:thumb:ring-2 focus:thumb:ring-primary-9"
         onChange={e => onChange(+e.target!.value)}
         {...props}
       />
